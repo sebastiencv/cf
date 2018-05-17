8b2fdc99bda1565cb4a7325fefd40141352bb579
Sebastien Cvilic <sebastien.cvilic@accenture.com>
2018-05-17 11:54:49 +0200

    add docker

---
+++ simple-docker/server.js
@@ -0,0 +1,16 @@
+/**
+ * Credit to https://github.com/minhajkk/mulkiya-ocr MIT
+ */
+
+const express = require('express')
+
+const app = express()
+
+app.use(express.static('public'))
+
+require('./routes')(app)
+
+const runningPortNumber = process.env.PORT || 3001
+const server = app.listen(runningPortNumber, () => {
+  console.log('Example app listening at http://%s:%s', server.address().address, server.address().port);
+})
