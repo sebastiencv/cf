bb6a379e4a3994a388b10aa20ae7347ca0b4ecc8
DIR\sebastien.cvilic <sebastien.cvilic@accenture.com>
2018-05-17 11:56:32 +0200

    add Microsoft cognitive services

--- ml-benchmark/test.js
+++ ml-benchmark/test.js
@@ -18,21 +18,28 @@ const apis = [{
   getText: (body) => {
     const data = JSON.parse(body)
     return data && data.predictions ? data.predictions[0] : ""
+  },
+}, {
+  name: 'ms-cognitive-service',
+  extension: 'jpeg',
+  url: 'https://westeurope.api.cognitive.microsoft.com/vision/v2.0/ocr?en&true',
+  headers: {
+    'Content-Type': 'multipart/form-data',
+    'Ocp-Apim-Subscription-Key': '3572dc0e141449e4a2631df847c0484d'
+  },
+  getText: (body) => {
+    const data = JSON.parse(body)
+    return getTextInObject(data)
   }
 }, {
-  name: 'cf-tesseract-3-jpeg',
+  name: 'cf-tesseract-3',
   extension: 'jpeg',
   url: 'https://app-seb.cfapps.eu10.hana.ondemand.com/api/ocr',
-  getText: (body) => body
-}, {
-  name: 'cf-tesseract-3-tiff',
-  extension: 'tiff',
-  url: 'https://app-seb.cfapps.eu10.hana.ondemand.com/api/ocr',
-  getText: (body) => body
+  getText: (body) => { return body }
 }]
 
 // concat 5 times
-const apisN = apis.concat(apis.concat(apis.concat(apis.concat(apis))))
+const apisN = apis.concat(apis.concat(apis))
 
 // call an API, return a promise
 const callApi = (api, fileName, imgPath) => {
@@ -64,20 +71,35 @@ const callApi = (api, fileName, imgPath) => {
   })
 }
 
-// reference text
-const fullText = "Human Resources Synergy 201, Bergman Street phone: + 31 5 66 999 888 fax: + 69 2 5664 445 Springfield, MO 12004 ssc.hr@synergy.com Emergency Contact List Dear Emily Clark, At Synergy we introduced last year a programme. In Case of Emergency, ie, firefighters, and police officers, as well as hospital personnel, to contact your next of kin to obtain important medical or support information. It is important for your safety in such event that the information available to us is up-to date. Below is a list of emergency contacts we have in our records: Relationship Contact Name Phone E-mail Wife Suzy User 999 645 2000 Please review this carefully and make any changes if needed. Yours Sincerely Tom Clark. Synergy Technology and Services Pvt. Ltd. 84, Wonderfull City Tel.: +91 80 33333333 Management: John Nash Hope Road Fax : + 91 80 33333333 Registered Ofﬁce: pentadore #123456. In the middle Web: www.synergy.corp E-mail:contactus@synergy.com Employee Reference: Emily Clark - admin Page: - of - "
-const fullTextLength = fullText.length
+const referenceText = "Human Resources Synergy 201, Bergman Street phone: + 31 5 66 999 888 fax: + 69 2 5664 445 Springfield, MO 12004 ssc.hr@synergy.com Emergency Contact List Dear Emily Clark, At Synergy we introduced last year a programme. In Case of Emergency, ie, firefighters, and police officers, as well as hospital personnel, to contact your next of kin to obtain important medical or support information. It is important for your safety in such event that the information available to us is up-to date. Below is a list of emergency contacts we have in our records: Relationship Contact Name Phone E-mail Wife Suzy User 999 645 2000 Please review this carefully and make any changes if needed. Yours Sincerely Tom Clark. Synergy Technology and Services Pvt. Ltd. 84, Wonderfull City Tel.: +91 80 33333333 Management: John Nash Hope Road Fax : + 91 80 33333333 Registered Ofﬁce: pentadore #123456. In the middle Web: www.synergy.corp E-mail:contactus@synergy.com Employee Reference: Emily Clark - admin Page: - of -"
 
 // compute precision between reference text and returned text
 // 100 means exact match (no distance)
 // 0 means as much permutation as chars
 const accuracy = (text) => {
-  return Math.round((fullTextLength - levenshtein(fullText, text.replace(/\\n/g, ' ').replace(/\s+/g, ' '))) / fullTextLength * 100)
+  const textSingleSpace = text.replace(/\\n/g, ' ').replace(/\s+/g, ' ')
+  // return Math.round((referenceTextLength - levenshtein(referenceText, textSingleSpace)) / referenceTextLength * 100)
+  const referenceWords = referenceText.split(" ")
+  const words = textSingleSpace.split(" ")
+  let countSuccess = 0
+  let countTotal = 0
+  // compute how much text words exists in reference
+  referenceWords.forEach(word => {
+    if (words.indexOf(word)!==-1) countSuccess++
+    countTotal++
+  })
+  // compute how much reference words exists in text
+  words.forEach(word => {
+    if (referenceWords.indexOf(word)!==-1) countSuccess++
+    countTotal++
+  })
+  return Math.round(countSuccess / countTotal * 100)
 }
 
 // process result, build stats
 let statsFile = 'url\tdpi\trotation\ttime\tprecision\n'
 const processResult = (api, {fileName, elapsedTime, body}) => {
+  debugger
   const currentAccuracy = accuracy(api.getText(body))
   const [,dpi,rotation] = fileName.match(/(\d*)dpi_r(\d*)/)
   // add stats
@@ -92,7 +114,7 @@ Promise.all(
   fs.readdirSync(path.join(__dirname, 'files'))
   // bypass system files
   .filter(file => file.indexOf('.') !== 0)
-  // .slice(0, 3)
+  // .slice(3, 4)
   // process files
   .map(file => {
     // loop each api
@@ -121,3 +143,17 @@ const keys = (items) => {
   })
 }
 
+// collect all 'text' items along the object structure
+const getTextInObject = (input) => {
+  const words = []
+  const getTextInObjectInner = (object) => {
+    if (object == null || typeof object !== 'object') return
+    Object.keys(object).map(key => {
+      if (key === 'text') words.push(object.text)
+      return getTextInObjectInner(object[key])
+    })
+  }
+  // search words
+  getTextInObjectInner(input)
+  return words.join(" ")
+}
