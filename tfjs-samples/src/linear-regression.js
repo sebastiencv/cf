1b412e5d84cca5f00fffc33dc4ef53c944ba2123
seb <sebastiencv@gmail.com>
2018-05-27 16:18:41 +0200

    add polynomial reg

--- tfjs-samples/src/linear-regression.js
+++ tfjs-samples/src/linear-regression.js
@@ -6,20 +6,17 @@ tf.setBackend('tensorflow')
 async function main() {
   // Create a simple model.
   const model = tf.sequential()
-  model.add(tf.layers.dense({units: 1, inputShape: [1]}))
+  model.add(tf.layers.dense({ units: 1, inputShape: [1] }))
 
   // Prepare the model for training: Specify the loss and the optimizer.
-  model.compile({
-    loss: 'meanSquaredError',
-    optimizer: 'sgd'
-  })
+  model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' })
 
   // Generate some synthetic data for training. (y = 2x - 1)
   const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1])
   const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1])
 
   // Train the model using the data.
-  await model.fit(xs, ys, {epochs: 250})
+  await model.fit(xs, ys, { epochs: 250 })
 
   // Use the model to do inference on a data point the model hasn't seen.
   // Should print approximately 39.
