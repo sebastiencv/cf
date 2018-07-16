126a310bbdded93ea55329ff9983e141e80296e6
seb <sebastiencv@gmail.com>
2018-07-16 18:48:35 +0200

    use tesseract-3

--- tesseract-seb/server.js
+++ tesseract-seb/server.js
@@ -6,6 +6,21 @@ const express = require('express')
 const tesseract = require('node-tesseract')
 const multer = require('multer')
 const fs = require('fs')
+const exec = require('child_process').exec
+
+// to run a shell command, will return a promise
+const cmd = (cmdStr) => {
+  return new Promise((resolve, reject) => {
+    exec(cmdStr, (error, stdout, stderr) => {
+      if (stderr && stderr.length > 0) console.log(`stderr: ${stderr}`)
+      if (error !== null) {
+        reject(error + stderr)
+      } else {
+        resolve(stdout)
+      }
+    })
+  })
+}
 
 // mime uploader
 const upload = multer({
@@ -26,23 +41,33 @@ const upload = multer({
  */
 const processRequest = (req, res) => {
   // console.log(req)
-  const path = req.file.path
-  const time = process.hrtime()
-  // Recognize text of any language in any format
-  tesseract.process(path, (err, text) => {
-    const diff = process.hrtime(time)
-    if (err) {
-      console.error(err)
-    } else {
-      fs.unlink(path, (err) => {
+  const restInputFile = req.file.path
+  const tesseractInputFile = `${restInputFile}.png`
+  const convertStart = process.hrtime()
+  cmd(`convert -density 300 ${restInputFile} -depth 8 -strip -background white -alpha off -append ${tesseractInputFile}`)
+    .then(() => {
+      const convertEnd = process.hrtime(convertStart)
+      // Recognize text of any language in any format
+      const tesseractStart = process.hrtime()
+      tesseract.process(tesseractInputFile, (err, text) => {
+        const tesseractEnd = process.hrtime(tesseractStart)
         if (err) {
-          res.status(500).json('Error while scanning image')
+          console.error(err)
+        } else {
+          fs.unlink(restInputFile, (err) => {
+            if (err) {
+              res.status(500).json('Error while scanning image')
+            }
+            console.log('successfully deleted %s', restInputFile)
+          })
+          res.status(200).json({
+            text: text,
+            convertRuntime: convertEnd[0] * 1000 + convertEnd[1] / 1000000,
+            tesseractRuntime: tesseractEnd[0] * 1000 + tesseractEnd[1] / 1000000
+          })
         }
-        console.log('successfully deleted %s', path)
       })
-      res.status(200).json({text: text, runtime: diff[0]*1000 + diff[1]/1000000})
-    }
-  })
+    })
 }
 
 // setup express routes
--- tesseract-seb/test.js
+++ tesseract-seb/test.js
@@ -1,55 +1,31 @@
-const express = require('express')
-const app = express()
-const serveIndex = require('serve-index')
-const path = require('path')
-const fs = require('fs-extra')
-const proxy = require('http-proxy-middleware')
+
 const tesseract = require('node-tesseract')
- 
-// provide folder and file browsing
-app.get(/^\/.*/, serveIndex(path.join(__dirname, './')))
-app.use(express.static(path.join(__dirname, './')))
 
-var sys = require('sys')
-var exec = require('child_process').exec;
+const exec = require('child_process').exec
 
-var cmd = (cmdStr) => {
-  exec(cmdStr, (error, stdout, stderr) => {
-    console.log(`== command : ${cmdStr} ==`)
-    console.log(`${stdout}`)
-    if (stderr && stderr.length > 0) console.log(`stderr: ${stderr}`)
-    if (error !== null) {
-      console.error(`exec error: ${error}`)
-    }
-  });
+const cmd = (cmdStr) => {
+  return new Promise((resolve, reject) => {
+    exec(cmdStr, (error, stdout, stderr) => {
+      if (stderr && stderr.length > 0) console.log(`stderr: ${stderr}`)
+      if (error !== null) {
+        reject(error + stderr)
+      } else {
+        resolve(stdout)
+      }
+    })
+  })
 }
 
-cmd(`ls -la $LD_LIBRARY_PATH`)
-cmd('echo $LD_LIBRARY_PATH')
-cmd(`ls -la $TESSDATA_PREFIX`)
-cmd('echo $TESSDATA_PREFIX')
-cmd(`ls /home/vcap/deps/0/tesseract-ocr`)
-cmd('echo $PATH')
-cmd(`pwd`)
 
-// tesseract processing
-app.all('/tesseract', function (req, res, next) {
-  console.log('Running tesseract ...')
-  tesseract.process(__dirname + '/image.jpg',function(err, text) {
-    if(err) {
+// cmd('gs -dNOPAUSE -q -sDEVICE=tiff12nc -r300 -dBATCH -sOutputFile=image.tif 2p.pdf')
+cmd('convert -density 300 2p.pdf -depth 8 -strip -background white -alpha off -append image.png')
+  .then(() => {
+    console.log('Running tesseract ...')
+    tesseract.process(__dirname + '/image.png', function (err, text) {
+      if (err) {
         console.error(err)
-        next() // pass control to the next handler
       } else {
         console.log(text)
-        res.send(text) // pass control to the next handler
-    }
-  });
-});
-  
-// Listen for requests
-const port = process.env.PORT || 3001;
-var server = app.listen(port, function() {
-  var port = server.address().port;
-  console.log(`Listen on port ${port}`);
-  console.log(`Try http://localhost:${port}/`);
-});
+      }
+    })
+  })
