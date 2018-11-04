90047cee5b0e98b89577b7023f6eba9115466ecf
sebastien.cvilic <sebastien.cvilic@accenture.com>
2018-11-04 20:57:56 +0100

    test simpleUI5 app

--- simpleUI5App/server.js
+++ simpleUI5App/server.js
@@ -1,10 +1,15 @@
-const express = require('express')
+var approuter = require("@sap/approuter")
 
-// setup express routes
-const app = express()
-app.use(express.static('webapp'))
-// start express
-const runningPortNumber = process.env.PORT || 3001
-const server = app.listen(runningPortNumber, () => {
-  console.log('SimpleUI5app listening at http://%s:%s', server.address().address, server.address().port)
-})
+var ar = approuter()
+ar.start()
+
+// const express = require('express')
+
+// // setup express routes
+// const app = express()
+// app.use(express.static('webapp'))
+// // start express
+// const runningPortNumber = process.env.PORT || 3001
+// const server = app.listen(runningPortNumber, () => {
+//   console.log('SimpleUI5app listening at http://%s:%s', server.address().address, server.address().port)
+// })
